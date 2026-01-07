4816f7ae18b58578df6d2a0ce93f7de35d86a63f
miguelmenezes17 <maniciomenezes@gmail.com>
2026-01-06 23:16:13 -0300

    Menu funcionando
    Menu funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -25,7 +25,7 @@ botao.addEventListener('click', () => {
 // Scroll suave para links de navegação
 const navLinks = document.querySelectorAll('#menu ul a.link');
 navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
